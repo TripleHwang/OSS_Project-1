6191e7fd205a4cb8927c96e3899c1358cf4d57be
TripleHwang <hwanghhh82@gmail.com>
2017-04-02 21:02:07 +0900

    sidebar making

--- memoproject/src/App.js
+++ memoproject/src/App.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from 'react-sidebar';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {sidebarOpen : false, sidebarDocked: false}
+  }
+
+  onSetSidebarOpen(open){
+    this.setState({sidebarOpen: open});
+  }
+
+  componentWillMount(){
+    var mql = window.matchMedia('(min-width: 800px)');
+    mql.addListener(this.mediaQueryChanged);
+    this.setState({mql: mql, sidebarDocked: mql.matches});
+  }
+
+  componentWillUnmount(){
+    this.state.mql.removeListener(this.mediaQueryChanged);
+  }
+
+  mediaQueryChanged(){
+    this.setState({sidebarDocked: this.state.mql.matches});
+  }
   render() {
+    var sidebarContent = <b>Sidebar content</b>;
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Sidebar sidebar={sidebarContent}
+               open={this.state.sidebarOpen}
+               docked={this.state.sidebarDocked}
+               onSetOpen={this.onSetSidebarOpen}>
+        <b>Main content</b>
+      </Sidebar>
     );
   }
 }
